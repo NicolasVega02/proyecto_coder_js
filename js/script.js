9ac7d3562043d8ba5e362f09267ff6cd96a86d38
Nicolas Vega <vegani44@gmail.com>
2023-05-31 20:37:08 -0300

    actualizacion 2da pre-entrega

--- js/script.js
+++ js/script.js
@@ -4,31 +4,72 @@
 
 alert("¡¿Cuanto sabes de One Piece?!");
 
-let usuario = prompt("Ingresa tu nombre futuro... ¡REY DE LOS PIRATAS!");
+let cantidadJugadores = parseInt( prompt("Ingrese la cantidad de jugadores"));
 
-while (!isNaN(usuario) || usuario == undefined) {
-    usuario = prompt("Valor incorrecto. Reingrese su nombre");
-};
+class jugadores {
+  constructor(nombre , recompensa) {
+    this.nombre = prompt("Ingresa tu nombre futuro... ¡REY DE LOS PIRATAS!");
+    this.edad = prompt("Ingrese su edad");
+    this.nacionalidad = prompt("Ingrese su país de origen");
+    this.recompensa = recompensa;
+  }
+}
+
+
 let correcto = 0;
 let incorrecto = 50;
 let berriesTotales = 0;
 let error;
+// let listaJugadores = [];
+// let jugador = new jugadores();
+let listaJugadores = [];
+
+for (let i = 0; i < cantidadJugadores ; i++) {
+    let jugador = new jugadores(0);
+    berriesTotales = 0;
+
+    primeraPregunta(jugador);
+    segundaPregunta(jugador);
+    terceraPregunta(jugador);
+    cuartaPregunta(jugador);
+    quintaPregunta(jugador);
+    sextaPregunta(jugador);
+    septimaPregunta(jugador);
+    octavaPregunta(jugador);
+    novenaPregunta(jugador);
+    decimaPregunta(jugador);
+    mostrarResultado(jugador);
+    
+    
+    console.log (jugador)
+    
+    console.log("Se busca " + jugador.nombre + " recompensa = " + berriesTotales + "  $berries");
+    
+    jugador.recompensa = berriesTotales;
+    listaJugadores.push(jugador);
+
+    
+    console.log (listaJugadores)
+}
+
+listaJugadores.sort((jugadorA, jugadorB) => jugadorB.recompensa - jugadorA.recompensa);
+
+console.log ("<-------------------------------------------------------------------------->")
+
+console.log("Recompensas ordenadas de mayor a menor");
+
+listaJugadores.forEach((jugador) => {
+  console.log(jugador);
+});
+
+let ranking = "";
+
+listaJugadores.forEach((jugador, index) => {
+  ranking += `${index + 1}. ${jugador.nombre} - Recompensa: ${jugador.recompensa} millones de berries\n`;
+});
 
 
-primeraPregunta();
-segundaPregunta();
-terceraPregunta();
-cuartaPregunta();
-quintaPregunta();
-sextaPregunta();
-septimaPregunta();
-octavaPregunta();
-novenaPregunta();
-decimaPregunta();
-mostrarResultado(); 
-
-
-
+alert("Ranking de jugadores por recompensa:\n"  + ranking);
 
 
 
@@ -36,11 +77,11 @@ mostrarResultado();
 
 // PREGUNTA 1
 
-function primeraPregunta(){
+function primeraPregunta(objeto){
     
     do {
         error = false;
-        let res = prompt("Pregunta 1: ¿Quien le dio a luffy el sombrero de paja? A - Monkey D Garp / B - Kosuki Oden / C - Silvers Rayleigh / D - Akagami Shanks.");
+        let res = prompt("Pregunta 1: ¿Quien le dio a luffy el sombrero de paja?\nA - Monkey D Garp \nB - Kosuki Oden \nC - Silvers Rayleigh \nD - Akagami Shanks.");
         let resMayuscula = res.toUpperCase();
         
         if(resMayuscula != "A" && resMayuscula != "B" && resMayuscula != "C" && resMayuscula != "D") {
@@ -51,12 +92,10 @@ function primeraPregunta(){
             if (resMayuscula == "D"){
                 correcto = 30000000
                 berriesTotales += correcto
-                console.log (berriesTotales)
-                alert ("Correcto! " + usuario + ", sigue sumando berries!")
+                alert ("Correcto! " + objeto.nombre + ", sigue sumando berries!")
             }else {
                 berriesTotales = berriesTotales + incorrecto
-                console.log (berriesTotales)
-                alert ("Incorrecto! " + usuario + ", mejor suerte en la siguiente pregunta!")
+                alert ("Incorrecto! " + objeto.nombre + ", mejor suerte en la siguiente pregunta!")
             }
         }
     } while (error)
@@ -65,10 +104,10 @@ function primeraPregunta(){
 
 //PREGUNTA 2 
 
-function segundaPregunta(){
+function segundaPregunta(objeto){
     do {
         error = false;
-        let res = prompt("Pregunta 2: ¿Quien es el primer nakama de luffy? A - Nami / B - Sanji / C - Zoro / D - Ussop.");
+        let res = prompt("Pregunta 2: ¿Quien es el primer nakama de luffy? \nA - Nami  \nB - Sanji  \nC - Zoro  \nD - Ussop.");
         let resMayuscula = res.toUpperCase();
         
         if(resMayuscula != "A" && resMayuscula != "B" && resMayuscula != "C" && resMayuscula != "D") {
@@ -79,10 +118,10 @@ function segundaPregunta(){
             if (resMayuscula == "C"){
                 correcto = 30000000;
                 berriesTotales += correcto;
-                alert ("Correcto! " + usuario + ", sigue sumando berries!")
+                alert ("Correcto! " + objeto.nombre + ", sigue sumando berries!")
             }else {
                 berriesTotales = berriesTotales + incorrecto
-                alert ("Incorrecto! " + usuario + ", mejor suerte en la siguiente pregunta!")
+                alert ("Incorrecto! " + objeto.nombre + ", mejor suerte en la siguiente pregunta!")
             }
         }
     } while (error)
@@ -90,11 +129,11 @@ function segundaPregunta(){
 
 //PREGUNTA 3
 
-function terceraPregunta(){
+function terceraPregunta(objeto){
     
     do {
         error = false;
-        let res = prompt("Pregunta 3: ¿Como se llama el estilo de pelea de Roronoa Zoro? A - Yontoryu / B - Santoryu / C ittoryu / D - Nitoryu.");
+        let res = prompt("Pregunta 3: ¿Como se llama el estilo de pelea de Roronoa Zoro? \nA - Yontoryu  \nB - Santoryu  \nC ittoryu  \nD - Nitoryu.");
         let resMayuscula = res.toUpperCase();
         
         if(resMayuscula != "A" && resMayuscula != "B" && resMayuscula != "C" && resMayuscula != "D") {
@@ -105,12 +144,12 @@ function terceraPregunta(){
             if (resMayuscula == "B"){
                 correcto = 30000000
                 berriesTotales += correcto
-                console.log (berriesTotales)
-                alert ("Correcto! " + usuario + ", sigue sumando berries!")
+                
+                alert ("Correcto! " + objeto.nombre + ", sigue sumando berries!")
             }else {
                 berriesTotales = berriesTotales + incorrecto
-                console.log (berriesTotales)
-                alert ("Incorrecto! " + usuario + ", mejor suerte en la siguiente pregunta!")
+                
+                alert ("Incorrecto! " + objeto.nombre + ", mejor suerte en la siguiente pregunta!")
             }
         }
     } while (error)
@@ -119,11 +158,11 @@ function terceraPregunta(){
 
 //PREGUNTA 4
 
-function cuartaPregunta(){
+function cuartaPregunta(objeto){
     
     do {
         error = false;
-        let res = prompt("Pregunta 4: ¿Cual es el nombre del rey de los piratas? A - Gold Roger / B - Monkey D Luffy / C - Edward Newgate / D - Gol D Roger.");
+        let res = prompt("Pregunta 4: ¿Cual es el nombre del rey de los piratas? \nA - Gold Roger  \nB - Monkey D Luffy  \nC - Edward Newgate  \nD - Gol D Roger.");
         let resMayuscula = res.toUpperCase();
         
         if(resMayuscula != "A" && resMayuscula != "B" && resMayuscula != "C" && resMayuscula != "D") {
@@ -134,12 +173,12 @@ function cuartaPregunta(){
             if (resMayuscula == "D"){
                 correcto = 30000000
                 berriesTotales += correcto
-                console.log (berriesTotales)
-                alert ("Correcto! " + usuario + ", sigue sumando berries!")
+                
+                alert ("Correcto! " + objeto.nombre + ", sigue sumando berries!")
             }else {
                 berriesTotales = berriesTotales + incorrecto
-                console.log (berriesTotales)
-                alert ("Incorrecto! " + usuario + ", mejor suerte en la siguiente pregunta!")
+                
+                alert ("Incorrecto! " + objeto.nombre + ", mejor suerte en la siguiente pregunta!")
             }
         }
     } while (error)
@@ -147,11 +186,11 @@ function cuartaPregunta(){
 
 //PREGUNTA 5
 
-function quintaPregunta(){
+function quintaPregunta(objeto){
     
     do {
         error = false;
-        let res = prompt("Pregunta 5: ¿En que isla conocieron a Franky el Cyborg? A - Wano Kuni / B - Dresrrosa / C - Water Seven / D - Punk Hazard.");
+        let res = prompt("Pregunta 5: ¿En que isla conocieron a Franky el Cyborg? \nA - Wano Kuni  \nB - Dresrrosa \nC - Water Seven  \nD - Punk Hazard.");
         let resMayuscula = res.toUpperCase();
         
         if(resMayuscula != "A" && resMayuscula != "B" && resMayuscula != "C" && resMayuscula != "D") {
@@ -162,12 +201,12 @@ function quintaPregunta(){
             if (resMayuscula == "C"){
                 correcto = 30000000
                 berriesTotales += correcto
-                console.log (berriesTotales)
-                alert ("Correcto! " + usuario + ", sigue sumando berries!")
+                
+                alert ("Correcto! " + objeto.nombre + ", sigue sumando berries!")
             }else {
                 berriesTotales = berriesTotales + incorrecto
-                console.log (berriesTotales)
-                alert ("Incorrecto! " + usuario + ", mejor suerte en la siguiente pregunta!")
+                
+                alert ("Incorrecto! " + objeto.nombre + ", mejor suerte en la siguiente pregunta!")
             }
         }
     } while (error)
@@ -176,11 +215,11 @@ function quintaPregunta(){
 
 //PREGUNTA 6
 
-function sextaPregunta(){
+function sextaPregunta(objeto){
     
     do {
         error = false;
-        let res = prompt("Pregunta 6: ¿Que habilidad brinda la Mera Mera no mi? A - Logia de Fuego / B - Logia de Magma / C - Logia de lodo / D - Zoan de Fenix. ");
+        let res = prompt("Pregunta 6: ¿Que habilidad brinda la Mera Mera no mi? \nA - Logia de Fuego  \nB - Logia de Magma  \nC - Logia de lodo  \nD - Zoan de Fenix. ");
         let resMayuscula = res.toUpperCase();
         
         if(resMayuscula != "A" && resMayuscula != "B" && resMayuscula != "C" && resMayuscula != "D") {
@@ -191,12 +230,12 @@ function sextaPregunta(){
             if (resMayuscula == "A"){
                 correcto = 100000000
                 berriesTotales += correcto
-                console.log (berriesTotales)
-                alert ("Correcto! " + usuario + ", sigue sumando berries!")
+                
+                alert ("Correcto! " + objeto.nombre + ", sigue sumando berries!")
             }else {
                 berriesTotales = berriesTotales + incorrecto
-                console.log (berriesTotales)
-                alert ("Incorrecto! " + usuario + ", mejor suerte en la siguiente pregunta!")
+                
+                alert ("Incorrecto! " + objeto.nombre + ", mejor suerte en la siguiente pregunta!")
             }
         }
     } while (error)
@@ -205,10 +244,10 @@ function sextaPregunta(){
 
 //PREGUNTA 7
 
-function septimaPregunta(){ 
+function septimaPregunta(objeto){ 
     do {
         error = false;
-        let res = prompt("Pregunta 7: ¿Como se llama la fruta del diablo que comio buggy el payaso? A - Gomu Gomu / B - ito ito / C - inu inu / D - bara bara ");
+        let res = prompt("Pregunta 7: ¿Como se llama la fruta del diablo que comio buggy el payaso? \nA - Gomu Gomu  \nB - ito ito  \nC - inu inu  \nD - bara bara ");
         let resMayuscula = res.toUpperCase();
         
         if(resMayuscula != "A" && resMayuscula != "B" && resMayuscula != "C" && resMayuscula != "D") {
@@ -219,12 +258,12 @@ function septimaPregunta(){
             if (resMayuscula == "D"){
                 correcto = 100000000
                 berriesTotales += correcto
-                console.log (berriesTotales)
-                alert ("Correcto! " + usuario + ", sigue sumando berries!")
+                
+                alert ("Correcto! " + objeto.nombre + ", sigue sumando berries!")
             }else {
                 berriesTotales = berriesTotales + incorrecto
-                console.log (berriesTotales)
-                alert ("Incorrecto! " + usuario + ", mejor suerte en la siguiente pregunta!")
+                
+                alert ("Incorrecto! " + objeto.nombre + ", mejor suerte en la siguiente pregunta!")
             }
         }
     } while (error)
@@ -232,11 +271,11 @@ function septimaPregunta(){
 
 //PREGUNTA 8
 
-function octavaPregunta(){
+function octavaPregunta(objeto){
     
     do {
         error = false;
-        let res = prompt("Pregunta 8: ¿Quien es el lider de la armada revolucionaria? A - Sabo / B - Monkey D Dragon / C - Portgas D Ace / D - Dr. Vegapunk.");
+        let res = prompt("Pregunta 8: ¿Quien es el lider de la armada revolucionaria? \nA - Sabo  \nB - Monkey D Dragon  \nC - Portgas D Ace  \nD - Dr. Vegapunk.");
         let resMayuscula = res.toUpperCase();
         
         if(resMayuscula != "A" && resMayuscula != "B" && resMayuscula != "C" && resMayuscula != "D") {
@@ -247,12 +286,12 @@ function octavaPregunta(){
             if (resMayuscula == "B"){
                 correcto = 100000000
                 berriesTotales += correcto
-                console.log (berriesTotales)
-                alert ("Correcto! " + usuario + ", sigue sumando berries!")
+                
+                alert ("Correcto! " + objeto.nombre + ", sigue sumando berries!")
             }else {
                 berriesTotales = berriesTotales + incorrecto
-                console.log (berriesTotales)
-                alert ("Incorrecto! " + usuario + ", mejor suerte en la siguiente pregunta!")
+                
+                alert ("Incorrecto! " + objeto.nombre + ", mejor suerte en la siguiente pregunta!")
             }
         }
     } while (error)
@@ -261,10 +300,10 @@ function octavaPregunta(){
 
 //PREGUNTA 9
 
-function novenaPregunta(){  
+function novenaPregunta(objeto){  
     do {
         error = false;
-        let res = prompt("Pregunta 9: ¿Cuales son la armas ancestrales? A - Urano, Pluton y Poseidon / B - Saturno, Pluton y Jupiter / C - Apollo, Venus y Poseidon / D - Pluton, Poseidon y Mercurio.");
+        let res = prompt("Pregunta 9: ¿Cuales son la armas ancestrales? \nA - Urano, Pluton y Poseidon  \nB - Saturno, Pluton y Jupiter  \nC - Apollo, Venus y Poseidon  \nD - Pluton, Poseidon y Mercurio.");
         let resMayuscula = res.toUpperCase();
         
         if(resMayuscula != "A" && resMayuscula != "B" && resMayuscula != "C" && resMayuscula != "D") {
@@ -275,12 +314,12 @@ function novenaPregunta(){
             if (resMayuscula == "A"){
                 correcto = 100000000
                 berriesTotales += correcto
-                console.log (berriesTotales)
-                alert ("Correcto! " + usuario + ", sigue sumando berries!")
+                
+                alert ("Correcto! " + objeto.nombre + ", sigue sumando berries!")
             }else {
                 berriesTotales = berriesTotales + incorrecto
-                console.log (berriesTotales)
-                alert ("Incorrecto! " + usuario + ", mejor suerte en la siguiente pregunta!")
+                
+                alert ("Incorrecto! " + objeto.nombre + ", mejor suerte en la siguiente pregunta!")
             }
         }
     } while (error)
@@ -289,11 +328,11 @@ function novenaPregunta(){
 
 //PREGUNTA 10
 
-function decimaPregunta(){
+function decimaPregunta(objeto){
     
     do {
         error = false;
-        let res = prompt("Pregunta 10: ¿Quien es el decimo capitan titanico de Barbanegra? A - Shiryu de la niebla / B - Jesus Burgess / C - Kuzan Aokiji / D - Avalo Pizarro.");
+        let res = prompt("Pregunta 10: ¿Quien es el decimo capitan titanico de Barbanegra? \nA - Shiryu de la niebla  \nB - Jesus Burgess  \nC - Kuzan Aokiji  \nD - Avalo Pizarro.");
         let resMayuscula = res.toUpperCase();
         
         if(resMayuscula != "A" && resMayuscula != "B" && resMayuscula != "C" && resMayuscula != "D") {
@@ -304,12 +343,12 @@ function decimaPregunta(){
             if (resMayuscula == "C"){
                 correcto = 100000000
                 berriesTotales += correcto
-                console.log (berriesTotales)
-                alert ("Correcto! " + usuario + ", sigue sumando berries!")
+                
+                alert ("Correcto! " + objeto.nombre + ", sigue sumando berries!")
             }else {
                 berriesTotales = berriesTotales + incorrecto
-                console.log (berriesTotales)
-                alert ("Incorrecto! " + usuario + ", mejor suerte en la siguiente pregunta!")
+                
+                alert ("Incorrecto! " + objeto.nombre + ", mejor suerte en la siguiente pregunta!")
             }
         }
     } while (error)
@@ -321,30 +360,30 @@ function decimaPregunta(){
 function mostrarResultado(){
     if (berriesTotales === 650000000){
         berriesTotales = berriesTotales + 4914800000
-        alert (" Felicidades tu recompensa es de " + berriesTotales + "$$ millones de Berries!! eres el REY DE LOS PIRATAS!")
+        alert (" Felicidades tu recompensa es de " + berriesTotales + "$$ millones de Berries!! \neres el REY DE LOS PIRATAS!")
     
     }else if (berriesTotales < 650000000 && berriesTotales >= 500000000){
         berriesTotales += 3000000000
-        alert (" Felicidades tu recompensa es de " + berriesTotales + "$$ millones de Berries!! eres un Emperador Pirata!")
+        alert (" Felicidades tu recompensa es de " + berriesTotales + "$$ millones de Berries!! \neres un Emperador Pirata!")
     
     }else if (berriesTotales < 500000000 && berriesTotales >= 400000000){
         berriesTotales += 1000000000
-        alert (" Felicidades tu recompensa es de " + berriesTotales + "$$ millones de Berries!! eres el Comandante de un  Emperador Pirata!")
+        alert (" Felicidades tu recompensa es de " + berriesTotales + "$$ millones de Berries!! \neres el Comandante de un  Emperador Pirata!")
     
     }else if (berriesTotales < 400000000 && berriesTotales >= 300000000){
-        alert (" Felicidades tu recompensa es de " + berriesTotales + "$$ millones de Berries!! eres un pirata muy buscado y en ascenso! debes cuidar tu espalda del gobierno mundial y los emperadores")
+        alert (" Felicidades tu recompensa es de " + berriesTotales + "$$ millones de Berries!! \neres un pirata muy buscado y en ascenso! debes cuidar tu espalda del gobierno mundial y los emperadores")
     
     }else if (berriesTotales < 300000000 && berriesTotales >= 100000000){
-        alert (" Felicidades tu recompensa es de " + berriesTotales + "$$ millones de Berries!! eres parte de los SUPERNOVA segun la marina la peor generacion de piratas desde hace 20 años! eres aun un novato pero de seguro te esperan grandes aventuras!")
+        alert (" Felicidades tu recompensa es de " + berriesTotales + "$$ millones de Berries!! \neres parte de los SUPERNOVA segun la marina la peor generacion de piratas desde hace 20 años! eres aun un novato pero de seguro te esperan grandes aventuras!")
     
     }else if (berriesTotales < 100000000 && berriesTotales >= 30000000){
-        alert (" Felicidades tu recompensa es de " + berriesTotales + "$$ millones de Berries!! eres un pirata con buena reputacion pero ese es tu techo, lo lamento T.T ")
+        alert (" Felicidades tu recompensa es de " + berriesTotales + "$$ millones de Berries!! \neres un pirata con buena reputacion pero ese es tu techo, lo lamento T.T ")
     
     }else if (berriesTotales < 30000000 && berriesTotales >= 500){
-        alert (" Tu recompensa es de " + berriesTotales + "$$ Berries!! lo que tienes de pirata lo tienes de guapo!")
+        alert (" Tu recompensa es de " + berriesTotales + "$$ Berries!! \nlo que tienes de pirata lo tienes de guapo!")
     }
-}
 
+}
 
 
 
